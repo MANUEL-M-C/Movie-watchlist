ae9bfa7e9f7f7bb55036d9cec01286a24ca984c8
MANUEL-M-C <manuelchandymanoj@gmail.com>
2026-08-17 23:50:33 +0530

    Connect Django to Render PostgreSQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,6 +5,7 @@ Django settings for config project.
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,10 +81,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
